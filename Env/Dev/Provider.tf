d3de8001c32586f1f64126ba380ec055729f326e
deepakkuma10038 <deepakkumar10038@gmail.com>
2026-01-19 12:09:15 +0530

    fix

--- Env/Dev/Provider.tf
+++ Env/Dev/Provider.tf
@@ -1,20 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.52.0"
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
     }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "deepak"
-    storage_account_name = "deep57stg"
-    container_name       = "deepcnt"
-    key                  = "terraform.tfstate"
   }
 }
 
-provider "azurerm" {
+ provider "azurerm" {
   features {}
   subscription_id = "2477fa1b-dc9b-4ee9-9ffb-56214b621aff"
 }
